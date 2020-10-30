c144d0e25a90633c9b78bd3bce9432ee6a6015b4
robertvenanzi <robertgvenanzi@gmail.com>
2020-10-29 23:00:34 -0700

    created menu and styles

---
+++ client/src/components/sections/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../styles/Menu.css'
+
+const Menu = () => {
+    return (
+        <div className='menu_sidebar'>
+            <ul>Dashboard</ul>
+            <ul>Leaderboard</ul>
+            <ul>Report</ul>
+        </div>
+    )
+}
+
+export default Menu
