9458a50acd1d862a9935c664e7e4abf55066ea39
robertvenanzi <robertgvenanzi@gmail.com>
2020-10-29 22:59:10 -0700

    created add styles

---
+++ client/src/components/sections/Add.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Symbol from '../../img/Symbol.png'
+import '../../styles/Add.css'
+
+const Add = () => {
+    return (
+        <div>
+            <img className='add_button' src={Symbol} alt="" />
+        </div>
+    )
+}
+
+export default Add
