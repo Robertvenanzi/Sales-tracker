7642eefa56ea992b43d82c5f73a56ea8ba93c620
robertvenanzi <robertgvenanzi@gmail.com>
2020-10-29 23:00:58 -0700

    created navbar and styles

---
+++ client/src/components/sections/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../../styles/Navbar.css'
+
+
+const Navbar = () => {
+    return (
+        <div className='header_container'>
+            <div className="nav_logo">
+                <div className='logo_span'>Sales<span>TRACKER</span> </div>
+            </div>
+            <div className='nav_links'>
+                <p>Home</p>
+                <p>Home</p>
+                <p>Home</p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
