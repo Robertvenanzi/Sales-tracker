71dcd1b093fef63d5646f6521ca010f56a4c06a5
robertvenanzi <robertgvenanzi@gmail.com>
2020-10-29 22:59:49 -0700

    created landing and styles

---
+++ client/src/components/layouts/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../styles/Landing.css'
+import Navbar from '../sections/Navbar';
+
+const Landing = () => {
+    return (
+        <div className='landing_body'>
+            <Navbar />
+            <div className='landing_hero'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Landing
